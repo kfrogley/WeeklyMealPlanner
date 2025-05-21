17243dcf77282a47d9b4af125a535d0f9ab31ecb
kfrogley <93785877+kfrogley@users.noreply.github.com>
2025-05-21 10:54:37 -0600

    Commit all files to main

---
+++ pantryItems.js
@@ -0,0 +1,21 @@
+const commonPantryItems = [
+    "all-purpose flour",
+    "granulated sugar",
+    "brown sugar",
+    "salt",
+    "black pepper",
+    "olive oil",
+    "vegetable oil",
+    "rice",
+    "dried pasta",
+    "canned tomatoes",
+    "canned beans",
+    "baking powder",
+    "baking soda",
+    "vinegar",
+    "soy sauce",
+    "chicken or vegetable broth",
+    "spice blends (e.g., Italian seasoning, cumin)"
+];
+
+module.exports = commonPantryItems;
